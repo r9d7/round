2248e6f0ef4e8e222af3d0806fa5a407dd627e73
Radu Dascalu <radu.dascalu@pm.me>
2024-10-11 15:16:12 +0100

    Order imports

--- components/ui/icon.tsx
+++ components/ui/icon.tsx
@@ -3,6 +3,7 @@ import SolarAltArrowLeftOutline from "~icons/solar/alt-arrow-left-outline";
 import SolarAltArrowRightOutline from "~icons/solar/alt-arrow-right-outline";
 import SolarArrowLeftDownOutline from "~icons/solar/arrow-left-down-outline";
 import SolarArrowRightUpOutline from "~icons/solar/arrow-right-up-outline";
+import SolarCard2Outline from "~icons/solar/card-2-outline";
 import SolarDownloadMinimalisticOutline from "~icons/solar/download-minimalistic-outline";
 import SolarGraphUpOutline from "~icons/solar/graph-up-outline";
 import SolarHomeSmileOutline from "~icons/solar/home-smile-outline";
@@ -10,7 +11,6 @@ import SolarInfoCircleOutline from "~icons/solar/info-circle-outline";
 import SolarMenuDotsBold from "~icons/solar/menu-dots-bold";
 import SolarRefreshOutline from "~icons/solar/refresh-outline";
 import SolarSuitcaseLinear from "~icons/solar/suitcase-linear";
-import SolarCard2Outline from "~icons/solar/card-2-outline";
 
 export const Icon = {
   home: { smile: { outline: SolarHomeSmileOutline } },
