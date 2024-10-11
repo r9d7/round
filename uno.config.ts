fb82cb9697d86b9e79a51acf72b8eb7637a5bcc7
Radu Dascalu <radu.dascalu@pm.me>
2024-10-11 08:35:07 +0100

    Install & setup UnoCSS

---
+++ uno.config.ts
@@ -0,0 +1,18 @@
+import {
+  defineConfig,
+  presetUno,
+  presetWebFonts,
+  transformerDirectives,
+  transformerVariantGroup,
+} from "unocss";
+
+export default defineConfig({
+  presets: [
+    presetUno(),
+    presetWebFonts({
+      provider: "bunny",
+      fonts: {},
+    }),
+  ],
+  transformers: [transformerVariantGroup(), transformerDirectives()],
+});
